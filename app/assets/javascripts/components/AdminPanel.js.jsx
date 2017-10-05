e7c6654333426a20fb536c97ac9d2623a279fd45
naigueldev <naiguelsantosdeveloper@gmail.com>
2017-10-04 21:55:28 -0300

    add navigation penes with react

---
+++ app/assets/javascripts/components/AdminPanel.js.jsx
@@ -0,0 +1,127 @@
+var AdminPanel = React.createClass({
+  render: function() {
+    return (
+      <h1>Its working!</h1>
+    );
+  }
+});
+
+React.DOM.render(
+    <AdminPanel />,
+    document.getElementById('content')
+);
+
+
+var Navigation = React.createClass({
+  getDefaultProps: function() {
+    return { 
+      tabList: [
+        {
+          id: 1,
+          title: "Manage Posts",
+          className: "active",
+          paneId: "manage-pane",
+          component: ManagePosts
+        },
+        {
+          id: 2,
+          title: "Create Post",
+          className: "",
+          paneId: "create-pane",
+          component: CreatePost
+        },
+        {
+          id: 3,
+          title: "Preview Post",
+          className: "",
+          paneId: "preview-pane",
+          component: PreviewPost
+        }
+      ]
+    }
+  },
+  render: function() {
+    return (
+      <div>
+        <NavTabs data={this.props.tabList}/>
+        <NavPanes data={this.props.tabList}/>
+      </div>
+    );
+  }
+});
+
+var ManagePosts = React.createClass({
+    render: function() {
+        return (
+            <p>ManagePosts Pane</p>
+        );
+    }
+});
+
+var CreatePost = React.createClass({
+    render: function() {
+        return (
+            <p>CreatePost Pane</p>
+        );
+    }   
+});
+
+var PreviewPost = React.createClass({
+    render: function() {
+        return (
+            <p>PreviewPost Pane</p>
+        );
+    }
+});
+
+var NavTabs = React.createClass({
+  render: function() {
+    var tabList = this.props.data.map(function(tab) {
+      return (
+        <Tab key={tab.id} data={tab} />
+      );
+    });
+    return (
+      <ul className="nav nav-tabs">
+        {tabList}
+      </ul>
+    );
+  }
+});
+
+var Tab = React.createClass({
+  render: function() {
+    return (
+      <li role="presentation" className={this.props.data.className}>
+        <a href={"#"+this.props.data.paneId} data-toggle="tab">
+          {this.props.data.title}
+        </a>
+      </li>  
+    );
+  }
+});
+
+var NavPanes = React.createClass({
+  render: function() {
+    var paneList = this.props.data.map(function(pane) {
+      return (
+        <Pane key={pane.id} data={pane} />
+      );
+    });
+    return (
+      <div className="tab-content">
+        {paneList}
+      </div>
+    );
+  }
+});
+
+var Pane = React.createClass({
+  render: function() {
+    return (
+      <div role="tabpanel" className={this.props.data.className} className={"tab-pane "+this.props.data.className} id={this.props.data.paneId}>
+        {this.props.data.title}
+      </div>
+    );
+  }
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 	root to: 'site#index'
 
+	get '/*path' => 'admin#index'
+
+	
 	namespace :api do
 		namespace :v1 do
 			resources :items, only: [:index, :create, :destroy, :update]
